e056825ed9c337fe9ab821c3498ceea69cacbc13
bgfa26 <bgfa26@gmail.com>
2018-09-30 01:40:06 -0400

    Primeros cambios sin estructura aun

---
+++ Braindo/app/src/main/java/com/app/braindo/braindo/LauncherActivity.java
@@ -0,0 +1,13 @@
+package com.app.braindo.braindo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
