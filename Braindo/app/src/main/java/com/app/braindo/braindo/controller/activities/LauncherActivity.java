a3868845df60d7d1858d5e0cf233acee80e183fd
bgfa26 <bgfa26@gmail.com>
2018-10-11 00:34:36 -0400

    Actualizacion Android

--- Braindo/app/src/main/java/com/app/braindo/braindo/controller/activities/LauncherActivity.java
+++ Braindo/app/src/main/java/com/app/braindo/braindo/controller/activities/LauncherActivity.java
@@ -1,7 +1,15 @@
 package com.app.braindo.braindo.controller.activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.app.braindo.braindo.R;
 
@@ -11,10 +19,30 @@ import java.util.List;
 public class LauncherActivity extends AppCompatActivity {
 
     public static List<Integer> itemPositionStacks = new ArrayList<>();
+    public TextView status_message;
+    static String str_result = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        new CountDownTimer(3000,1000){
+            @Override
+            public void onTick(long millisUntilFinished){}
+
+            @Override
+            public void onFinish(){
+                //set the new Content of your activity
+                    try {
+                        final Intent myintent = new Intent(LauncherActivity.this, StartUpActivity.class);
+                        boolean connected = false;
+                        startActivity(myintent);
+                    }catch(Exception ex){
+                        ex.getStackTrace();
+                    }
+            }
+        }.start();
     }
 }
