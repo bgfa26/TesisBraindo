683ed7ae82c06c671925af5a8bb95eac61ff9c28
bgfa26 <bgfa26@gmail.com>
2018-10-17 23:03:04 -0400

    Cambios encuesta

--- Braindo/app/src/main/java/com/app/braindo/braindo/controller/activities/RegistrationActivity.java
+++ Braindo/app/src/main/java/com/app/braindo/braindo/controller/activities/RegistrationActivity.java
@@ -1,8 +1,11 @@
 package com.app.braindo.braindo.controller.activities;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.app.braindo.braindo.R;
@@ -18,7 +21,7 @@ public class RegistrationActivity extends AppCompatActivity{
     public static List<Integer> itemPositionStacks = new ArrayList<>();
     public TextView status_message;
     static String str_result = "";
-
+    private Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +29,24 @@ public class RegistrationActivity extends AppCompatActivity{
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_registration);
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            testButton = (Button) findViewById(R.id.btnRegistration);
+
+            testButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    moveToTest();
+                }
+
+            });
+
         }catch(Exception ex){
             ex.getStackTrace();
         }
+    }
+
+    private void moveToTest(){
+        Intent myintent = new Intent (RegistrationActivity.this, TestActivity.class);
+        finish();
+        startActivity(myintent);
     }
 }
