471d622588e47b2d23e0f2d1cb772ec5a64bbfd6
Barbara Fernandez <bgfa26@gmail.com>
2018-11-07 22:28:02 -0400

    Master Page para los menu

--- BraindoWeb/Braindo/View/MenuLayout.Master.designer.cs
+++ BraindoWeb/Braindo/View/MenuLayout.Master.designer.cs
@@ -10,24 +10,24 @@
 namespace Braindo.View {
     
     
-    public partial class index {
+    public partial class MenuLayout {
         
         /// <summary>
-        /// gridcss control.
+        /// head control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlLink gridcss;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
-        /// form1 control.
+        /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
